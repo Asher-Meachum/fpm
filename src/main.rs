05c85c717e5c7f023ef10e0183bde77a0c6c311b
Asher Bernstein-Meachum <dev.asherbm.untracked946@passinbox.com>
2026-06-28 07:16:40 -0500

    Refactor Commands::Update branch into functions

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,31 @@ use clap::Parser;
 use crate::argparse::{Cli, Commands};
 use crate::config::Config;
 
+fn filter(list: Vec<types::Link>, filters: Vec<Vec<impl ToString>>) -> Vec<types::Link> {
+    let mut list = list;
+
+    for item in filters {
+        let item_names: Vec<String> = item.iter().map(|l| l.to_string()).collect();
+        list.retain(|l| !item_names.contains(&l.name()));
+    }
+
+    list
+}
+
+fn overwrite_nonexistent(b: bool, links: &Vec<types::Link>) -> bool {
+    if b {
+        match interface::overwrite_with_nonexistent(links) {
+            Ok(b) => b,
+            Err(_) => {
+                eprintln!("Error reading input. Not overwriting");
+                false
+            }
+        }
+    } else {
+        false
+    }
+}
+
 fn action() -> Result<(), types::Error> {
     let cli = Cli::parse();
 
@@ -47,37 +72,29 @@ fn action() -> Result<(), types::Error> {
             Ok(())
         }
         Commands::Update => {
-            let mut changed: u64 = 0;
-
-            let mut final_links = config.links().clone();
+            let mut filter_list = Vec::new();
 
             // Remove links listed by --ignore
             if let Some(l) = cli.ignore {
-                let ignore = l.split(",").collect::<Vec<&str>>();
-                final_links.retain(|link| !ignore.contains(&link.name().as_str()));
+                let ignore = l.split(",").map(|n| n.to_string()).collect::<Vec<String>>();
+                filter_list.push(ignore);
             }
 
-            let upstream_doesnt_exist = sync::upstream_not_exists(&final_links);
-            let overwrite_with_nonexistent = {
-                if cli.force {
-                    true
-                } else {
-                    match interface::overwrite_with_nonexistent(&upstream_doesnt_exist) {
-                        Ok(b) => b,
-                        Err(_) => {
-                            eprintln!("Error reading input. Not overwriting");
-                            false
-                        }
-                    }
-                }
-            };
+            let upstream_doesnt_exist = sync::upstream_not_exists(config.links());
 
             // Removes links that the upstream file doesn't exist, if overwite is set to disable.
-            if !overwrite_with_nonexistent {
-                final_links.retain(|link| !&upstream_doesnt_exist.contains(link));
+            if !overwrite_nonexistent(cli.force, &upstream_doesnt_exist) {
+                filter_list.push(
+                    upstream_doesnt_exist
+                        .iter()
+                        .map(|l| l.name())
+                        .collect::<Vec<String>>(),
+                );
             }
 
-            let result = sync::update(&final_links);
+            let result = sync::update(&filter(config.links().to_owned(), filter_list));
+
+            let mut changed: u64 = 0;
 
             for r in result {
                 println!("{}", r);
